a285983cdb85c47e0b55489010b9e9562a72dfe5
Dobledroid <carlosexequiel360@gmail.com>
2024-06-24 19:58:29 -0600

    update

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ import pandas as pd
 app = Flask(__name__)
 
 # Cargar el modelo entrenado
-model = joblib.load('modelo_clima_regresion.pkl')
-scaler = joblib.load('standard_scaler.pkl')
+model = joblib.load('modelo_clima_regresion_v2.pkl')
+scaler = joblib.load('standard_scaler_v2.pkl')
 
 @app.route('/')
 def home():
@@ -18,14 +18,14 @@ def predict():
         # Obtener los datos enviados en el request
         avg_humidity = float(request.form['avg_humidity'])
         avg_dewpoint = float(request.form['avg_dewpoint'])
-        min_pressure = float(request.form['min_pressure'])
-        diff_pressure = float(request.form['diff_pressure'])
+        avg_barometer = float(request.form['avg_barometer'])
+        max_pressure = float(request.form['max_pressure'])
         
         # Escalar los datos de entrada
-        scaled_data = scaler.transform([[avg_humidity, avg_dewpoint, min_pressure, diff_pressure]])
+        scaled_data = scaler.transform([[avg_humidity, avg_dewpoint, avg_barometer, max_pressure]])
         
         # Crear un DataFrame con los datos escalados
-        data_df = pd.DataFrame(scaled_data, columns=['Average humidity (%)', 'Average dewpoint (°F)', 'Minimum pressure', 'diff_pressure'])
+        data_df = pd.DataFrame(scaled_data, columns=['Average humidity (%)', 'Average dewpoint (°F)', 'Average barometer (in)', 'Maximum pressure'])
         
         # Imprimir el DataFrame para verificar los datos
         print("Datos recibidos (escalados):")
@@ -39,5 +39,6 @@ def predict():
     
     except Exception as e:
         return jsonify({'error': str(e)}), 400
+
 if __name__ == '__main__':
     app.run(debug=True)
